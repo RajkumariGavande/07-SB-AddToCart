69cf6c9d6c74e747d931c7503576d43d20798086
RajkumariGavande <sakshigavande352@gmail.com>
2025-12-06 14:28:00 +0530

    Variable Added

--- src/main/java/com/cjc/app/model/Product.java
+++ src/main/java/com/cjc/app/model/Product.java
@@ -13,6 +13,9 @@ import org.hibernate.annotations.DynamicUpdate;
 @DynamicUpdate
 @Entity
 public class Product {
+
+         private String name="Ronit";
+
 	@Id
 	@Column(name = "PRODUCT_ID")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
